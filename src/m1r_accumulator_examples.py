1a5860a5c51f285c8f3c857802300d7d59b243ad
wilcoxeb <wilcoxeb@rose-hulman.edu>
2018-12-03 14:14:21 -0500

    m1 session 4

--- src/m1r_accumulator_examples.py
+++ src/m1r_accumulator_examples.py
@@ -5,12 +5,12 @@ This module demonstrates the ACCUMULATOR pattern in three classic forms:
    IN GRAPHICS:   x = x + pixels
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Emily Wilcox.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
 #
-# TODO: 2.
+# Done: 2.
 #   RUN this program, then READ its code.
 #     Then answer the following, GETTING HELP AS NEED!  (Ask questions!!!)
 #     Write your answers in any reasonable way (your choice).
@@ -26,39 +26,44 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
 #     a. If you want a loop that runs   r   times,
 #          which of the following three choices would you use?
 #
-#            for k in range(r - 1):
-#            for k in range(r):
-#            for k in range(r + 1):
+#            a) for k in range(r - 1):
+#            b) for k in range(r):
+#            c) for k in range(r + 1):
+#
+#               answer: b
 #
 #     b. If you want a loop that runs from 0 to s, inclusive,
 #          what expression would you use in the _____ below?
 #
 #            for k in range(_____):
+#               answer: s + 1
 #
 #     c. If you want a loop that runs from r to s, inclusive, assuming s >= r,
 #          what expression would you use in the _____ below?
 #
 #            for k in range(_____):
+#               answer: (s + 1) - r
 #
 #     d. If you want a loop that runs from (r + 4) to (s - 10),
 #          including the (r + 4) but not including the (s - 10),
 #          what expression would you use in the _____ below?
 #
 #            for k in range(_____):
+#               answer: (s - 10) - (r + 4)
 #
 #     e. The following code snippet attempts to return the number
 #          of integers from r to s, inclusive, whose cosines are positive.
 #          It has at least 5 distinct errors (one per line).  What are they?
 #
-#              for k in range(r - s):
-#                  count = 0
-#                  if math.cos(r) > 0:
-#                      count = 1
-#                  return count
+#              for k in range(r - s):       should be (s+1)-r
+#                  count = 0                should be before loop
+#                  if math.cos(r) > 0:      should be r + k
+#                      count = 1            should be count = count + 1
+#                  return count             should be after loop
 #
 #     f. The code in the "graphics accumulation" example below includes:
 #            for _ in range(n):
-#        What does the   _   (underscore) mean?
+#        What does the   _   (underscore) mean? A loop variable not used in a loop
 #
 #     g. The code in the "graphics accumulation" example below includes:
 #
@@ -72,7 +77,7 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
 #        If you want the row-of-circles that the above creates,
 #        one of the following two attempts is a CORRECT attempt
 #        (i.e., is equivalent in its functionality to the above)
-#        and one is WRONG.  Which is the WRONG one?
+#        and one is WRONG.  Which is the WRONG one? The bottom one os wrong because it tries to use k and the x and y's
 #
 #              x = starting_point.x
 #              for k in range(n):
@@ -330,4 +335,8 @@ def draw_row_of_circles(n, starting_point, color, window):
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
+
+
 main()
+
+
